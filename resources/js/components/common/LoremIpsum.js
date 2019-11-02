6ea7b077c7684fd471969723bb350c98289f7870
programarivm <info@programarivm.com>
2019-11-02 10:38:30 +0000

    lorem ipsum text is added

--- resources/js/components/common/LoremIpsum.js
+++ resources/js/components/common/LoremIpsum.js
@@ -6,6 +6,7 @@ class LoremIpsum extends React.Component {
     return (
       <Jumbotron className="mt-3">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
       </Jumbotron>
     );
   }
