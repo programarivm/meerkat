82ac1d361ae8ec3f7f941a4038cc2440c178d4b2
programarivm <info@programarivm.com>
2019-12-24 10:48:02 +0000

    DELETE /api/reviews/{review} passes the tests

---
+++ tests/api/reviews/DeleteTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Api\Reviews;
+
+use App\User;
+use Tests\api\AuthenticatedTestCase;
+
+class DeleteTest extends AuthenticatedTestCase
+{
+    /**
+     * @dataProvider data
+     * @test
+     */
+    public function delete($review)
+    {
+        $response = $this->call('DELETE', "/api/reviews/$review", [], ['access_token' => $this->cookies->access_token]);
+
+        switch ($this->cookies->session->role) {
+            case User::CHOICE_ROLE_BASIC:
+                $response->assertStatus(403);
+                break;
+            case User::CHOICE_ROLE_EDITOR:
+                $response->assertStatus(200);
+                break;
+            case User::CHOICE_ROLE_ADMIN:
+                $response->assertStatus(200);
+                break;
+            default:
+                $this->assertTrue(false);
+                break;
+        }
+    }
+
+    public function data()
+    {
+        $data = [];
+        $queryStrings = json_decode(file_get_contents(__DIR__ . '/data/delete_http_status_200.json'))->queryString;
+        foreach ($queryStrings as $queryString) {
+            $data[] = [
+                $queryString->review
+            ];
+        }
+
+        return $data;
+    }
+}
