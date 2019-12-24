bc2fd934dd37ce0d7c029a5aa7d27e2975dd0cfb
programarivm <info@programarivm.com>
2019-12-24 14:21:37 +0000

    sample testing data is added, and DELETE /api/reviews/{review} passes the tests

--- tests/api/reviews/DeleteTest.php
+++ tests/api/reviews/DeleteTest.php
@@ -31,10 +31,47 @@ class DeleteTest extends AuthenticatedTestCase
         }
     }
 
+    /**
+     * @dataProvider data_delete_200
+     * @test
+     */
+    public function delete_404($review)
+    {
+        $response = $this->call('DELETE', "/api/reviews/$review", [], ['access_token' => $this->cookies->access_token]);
+
+        switch ($this->cookies->session->role) {
+            case User::CHOICE_ROLE_BASIC:
+                $response->assertStatus(403);
+                break;
+            case User::CHOICE_ROLE_EDITOR:
+                $response->assertStatus(404);
+                break;
+            case User::CHOICE_ROLE_ADMIN:
+                $response->assertStatus(404);
+                break;
+            default:
+                $this->assertTrue(false);
+                break;
+        }
+    }
+
     public function data_delete_200()
     {
         $data = [];
         $queryStrings = json_decode(file_get_contents(__DIR__ . '/data/delete_200.json'))->queryString;
+        foreach ($queryStrings as $queryString) {
+            $data[] = [
+                $queryString->review,
+            ];
+        }
+
+        return $data;
+    }
+
+    public function data_delete_404()
+    {
+        $data = [];
+        $queryStrings = json_decode(file_get_contents(__DIR__ . '/data/delete_404.json'))->queryString;
         foreach ($queryStrings as $queryString) {
             $data[] = [
                 $queryString->review,
