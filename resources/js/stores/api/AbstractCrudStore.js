f5be5e54072d432e32d3b0ad353ff50286a516a3
programarivm <info@programarivm.com>
2019-12-26 21:29:44 +0000

    AbstractCrudStore is been implemented

---
+++ resources/js/stores/api/AbstractCrudStore.js
@@ -0,0 +1,134 @@
+import { EventEmitter } from 'events';
+
+const errorMessage = 'Whoops! Sorry there was an error, please try again later.';
+
+export default class AbstractCrudStore extends EventEmitter {
+
+	create(data) {
+		fetch(process.env.MIX_APP_URL + this.endpoint, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+		.then((res) => {
+			switch (res.status) {
+				case 201:
+					this.emit("create.201");
+					break;
+				case 422:
+					res.json().then((data) => {
+						let validation = [];
+	          Object.values(data.errors).forEach(value => {
+	            value.forEach(message => {
+	              validation.push(message);
+	            });
+	          });
+						this.emit("create.422", validation);
+					});
+					break;
+				default:
+					this.emit("create.error", [errorMessage]);
+					break;
+			}
+		});
+	}
+
+	delete(id) {
+		fetch(process.env.MIX_APP_URL + this.endpoint + `/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+		.then((res) => {
+			switch (res.status) {
+				case 204:
+					this.emit("delete.204");
+					break;
+				default:
+					this.emit("delete.error", [errorMessage]);
+					break;
+			}
+		});
+	}
+
+	fetchAll() {
+		fetch(process.env.MIX_APP_URL + this.endpoint, {
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+		.then((res) => {
+			switch (res.status) {
+				case 200:
+					res.json().then((data) => {
+						this.emit("fetch_all.200", data);
+					});
+					break;
+				default:
+					this.emit("fetch_all.error", [errorMessage]);
+					break;
+			}
+		});
+	}
+
+	show(id) {
+		fetch(process.env.MIX_APP_URL + this.endpoint + `/${id}`, {
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+		.then((res) => {
+			switch (res.status) {
+				case 200:
+					res.json().then((data) => {
+						this.emit("show.200", data);
+					});
+					break;
+				default:
+					this.emit("show.error", [errorMessage]);
+					break;
+			}
+		});
+	}
+
+	update(id, data) {
+		fetch(process.env.MIX_APP_URL + this.endpoint + `/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+		.then((res) => {
+			switch (res.status) {
+				case 200:
+					this.emit("update.200");
+					break;
+				case 422:
+					res.json().then((data) => {
+						let validation = [];
+	          Object.values(data.errors).forEach(value => {
+	            value.forEach(message => {
+	              validation.push(message);
+	            });
+	          });
+						this.emit("update.422", validation);
+					});
+					break;
+				default:
+					this.emit("update.error", [errorMessage]);
+					break;
+			}
+		});
+	}
+}
--- resources/js/stores/api/ReviewStore.js
+++ resources/js/stores/api/ReviewStore.js
@@ -1,87 +1,12 @@
 import ApiReviewActionTypes from '../../constants/api/ReviewActionTypes';
 import ApiReviewDispatcher from "../../dispatcher/api/ReviewDispatcher.js";
-import { EventEmitter } from 'events';
+import AbstractCrudStore from "./AbstractCrudStore.js";
 
-const errorMessage = 'Whoops! Sorry there was an error, please try again later.';
-
-class ReviewStore extends EventEmitter {
+class ReviewStore extends AbstractCrudStore {
 	constructor() {
 		super();
+		this.endpoint = '/api/reviews';
 		ApiReviewDispatcher.register(this.handleActions.bind(this));
-	}
-
-	create(data) {
-		fetch(process.env.MIX_APP_URL + '/api/reviews', {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
-		.then((res) => {
-			switch (res.status) {
-				case 201:
-					this.emit("create.201");
-					break;
-				case 422:
-					res.json().then((data) => {
-						let validation = [];
-	          Object.values(data.errors).forEach(value => {
-	            value.forEach(message => {
-	              validation.push(message);
-	            });
-	          });
-						this.emit("create.422", validation);
-					});
-					break;
-				default:
-					this.emit("create.error", [errorMessage]);
-					break;
-			}
-		});
-	}
-
-	delete(id) {
-		fetch(process.env.MIX_APP_URL + `/api/reviews/${id}`, {
-			method: 'DELETE',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			}
-		})
-		.then((res) => {
-			switch (res.status) {
-				case 204:
-					this.emit("delete.204");
-					break;
-				default:
-					this.emit("delete.error", [errorMessage]);
-					break;
-			}
-		});
-	}
-
-	fetchAll() {
-		fetch(process.env.MIX_APP_URL + '/api/reviews', {
-			method: 'GET',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			}
-		})
-		.then((res) => {
-			switch (res.status) {
-				case 200:
-					res.json().then((data) => {
-						this.emit("fetch_all.200", data);
-					});
-					break;
-				default:
-					this.emit("fetch_all.error", [errorMessage]);
-					break;
-			}
-		});
 	}
 
 	handleActions(action) {
