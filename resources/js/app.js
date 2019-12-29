d58574279b7d211558b549f82dae95b082c889ae
programarivm <info@programarivm.com>
2019-12-29 20:41:59 +0000

    PACE is removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import Meerkat from './Meerkat.js';
 import { BrowserRouter } from "react-router-dom";
 
-import 'pace-js';
-
-import 'pace-js/themes/blue/pace-theme-center-simple.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-table/react-table.css';
 
