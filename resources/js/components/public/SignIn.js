dcdc7d4061eed6e6a29ce36798b26f39327ec81f
programarivm <info@programarivm.com>
2020-04-26 15:47:57 +0100

    the SignIn component design is updated

--- resources/js/components/public/SignIn.js
+++ resources/js/components/public/SignIn.js
@@ -2,7 +2,7 @@ import ability from '../../ability';
 import abilityRules from '../../../../storage/ability-rules.json';
 import ApiAuthActions from '../../actions/api/AuthActions';
 import ApiAuthStore from '../../stores/api/AuthStore';
-import { Button, Divider, Grid, TextField, Typography } from '@material-ui/core';
+import { Button, Divider, Grid, Paper, TextField, Typography } from '@material-ui/core';
 import Loading from '../Loading';
 import React from 'react';
 import Session from '../../Session';
@@ -53,53 +53,53 @@ class SignIn extends React.Component {
       <Grid container style={{ margin: 20, padding: 20 }}>
         <Grid item xs={4}></Grid>
         <Grid item xs={4}>
-          <Typography component="h1" variant="h5">
-            Log in to your account
-          </Typography>
-          <Loading loading={this.state.loading}>
-            <Validation show={true} messages={this.state.response} />
-          </Loading>
-          <form onSubmit={ (e) => this.handleLogin(e) }>
-            <TextField
-              required
-              fullWidth
-              autoFocus
-              variant="outlined"
-              margin="normal"
-              id="email"
-              label="Email address"
-              name="email"
-              autoComplete="email"
-            />
-            <TextField
-              required
-              fullWidth
-              variant="outlined"
-              margin="normal"
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              style={{ marginTop: 10 }}
-            >
-              Log in
-            </Button>
-          </form>
-          <Divider style={{ margin: 20 }} />
-          <p>How to use the demo:</p>
-          <ul>
-            <li>john@gmail.com as a basic user</li>
-            <li>alice@gmail.com as an editor user</li>
-            <li>bob@gmail.com as an admin user</li>
-          </ul>
-          <p>The password for all roles is <code>password</code></p>
+          <Paper style={{ padding: 15 }}>
+            <Typography align="center" variant="h6">
+              Log in to your account
+            </Typography>
+            <Loading loading={this.state.loading}>
+              <Validation show={true} messages={this.state.response} />
+            </Loading>
+            <form onSubmit={ (e) => this.handleLogin(e) }>
+              <TextField
+                required
+                fullWidth
+                autoFocus
+                margin="normal"
+                id="email"
+                label="Email address"
+                name="email"
+                autoComplete="email"
+              />
+              <TextField
+                required
+                fullWidth
+                margin="normal"
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                style={{ marginTop: 10 }}
+              >
+                Log in
+              </Button>
+            </form>
+            <Divider style={{ margin: 20 }} />
+            <p>How to use the demo:</p>
+            <ul>
+              <li>john@gmail.com as a basic user</li>
+              <li>alice@gmail.com as an editor user</li>
+              <li>bob@gmail.com as an admin user</li>
+            </ul>
+            <p>The password for all roles is <code>password</code></p>
+          </Paper>
         </Grid>
         <Grid item xs={4}></Grid>
       </Grid>
