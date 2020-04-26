9244a0ca68cac248afa0a66d4108ac2bad20a74c
programarivm <info@programarivm.com>
2020-04-26 11:19:12 +0100

    reactstrap is replaced with Material UI in the main navigation bar

--- resources/js/components/private/MainNav.js
+++ resources/js/components/private/MainNav.js
@@ -1,79 +1,76 @@
+import { AppBar, Button, ButtonGroup, Toolbar } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import Can from '../Can';
-import {
-  Collapse, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar,
-  NavbarToggler, NavbarBrand, NavItem, NavLink, UncontrolledDropdown } from 'reactstrap';
-import { Link, NavLink as RouterNavLink, Route } from 'react-router-dom';
+import { Link, NavLink, Route } from 'react-router-dom';
+import MyAccountMenu from './MyAccountMenu';
 import React from 'react';
 import { Restaurants } from './Restaurants';
 import { Reviews } from '../common/Reviews';
 import { SignOut } from './SignOut';
 import { Users } from './Users';
 import logo from '../../../images/logo.png';
-import '../common/MainNav.css';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuOption: {
+    marginRight: theme.spacing(5),
+    color: '#fff',
+    '&:hover': {
+       color: '#fff',
+    },
+  },
+  menuOptionActive: {
+    fontWeight: 'bold',
+  },
+  buttonGroup: {
+    backgroundColor: '#fff',
+    marginLeft: 'auto',
+  },
+});
 
 class MainNav extends React.Component {
-  _isMounted = false;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  toggle() {
-    if (this._isMounted) {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
-  }
-
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Navbar light expand="md" className="MainNav">
-          <NavbarBrand tag={Link} to="/reviews">
-            <img src={logo} className="logo" alt="logo" />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={RouterNavLink} to="/reviews" activeClassName="active">Reviews</NavLink>
-              </NavItem>
-              <Can I="index" a="User">
-                <NavItem>
-                  <NavLink tag={RouterNavLink} to="/users" activeClassName="active">Users</NavLink>
-                </NavItem>
-              </Can>
-              <Can I="update" a="Restaurant">
-                <NavItem>
-                  <NavLink tag={RouterNavLink} to="/restaurants" activeClassName="active">Restaurants</NavLink>
-                </NavItem>
-              </Can>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  My account
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    <NavLink tag={RouterNavLink} to="/logout">Sign out</NavLink>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <img src={logo} alt="Warthog logo" className={classes.menuOption} />
+            <NavLink
+              tag={NavLink}
+              to="/reviews"
+              className={classes.menuOption}
+              activeClassName={classes.menuOptionActive}
+            >
+              Reviews
+            </NavLink>
+            <Can I="index" a="User">
+              <NavLink
+                tag={NavLink}
+                to="/users"
+                className={classes.menuOption}
+                activeClassName={classes.menuOptionActive}
+              >
+                Users
+              </NavLink>
+            </Can>
+            <Can I="update" a="Restaurant">
+              <NavLink
+                tag={NavLink}
+                to="/restaurants"
+                className={classes.menuOption}
+                activeClassName={classes.menuOptionActive}
+              >
+                Restaurants
+              </NavLink>
+            </Can>
+            <ButtonGroup size="small" aria-label="small outlined button group" className={classes.buttonGroup}>
+              <MyAccountMenu />
+            </ButtonGroup>
+          </Toolbar>
+        </AppBar>
         <Route
           path="/reviews"
           render={(props) => <Reviews {...props} />}
--- resources/js/components/public/MainNav.js
+++ resources/js/components/public/MainNav.js
@@ -1,63 +1,75 @@
-import {
-  Collapse, Nav, Navbar, NavbarToggler,
-  NavbarBrand, NavItem } from 'reactstrap';
-import { Home } from "./Home";
+import { AppBar, Button, ButtonGroup, Toolbar } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { Home } from './Home';
 import { Link, NavLink, Route, Redirect } from 'react-router-dom';
 import React from 'react';
 import { Reviews } from '../common/Reviews';
-import { SignIn } from './SignIn';
+import SignIn from './SignIn';
 import logo from '../../../images/logo.png';
-import '../common/MainNav.css';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuOption: {
+    marginRight: theme.spacing(5),
+    color: '#fff',
+    '&:hover': {
+       color: '#fff',
+    },
+  },
+  menuOptionActive: {
+    fontWeight: 'bold',
+  },
+  buttonGroup: {
+    backgroundColor: '#fff',
+    marginLeft: 'auto',
+  },
+});
 
 class MainNav extends React.Component {
-  _isMounted = false;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  toggle() {
-    if (this._isMounted) {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
-  }
-
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Navbar light expand="md" className="MainNav">
-          <NavbarBrand tag={Link} to="/home">
-            <img src={logo} className="logo" alt="logo" />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={Link} to="/home">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/reviews">Reviews</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/login">Sign In</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <NavLink
+              tag={Link}
+              to="/home"
+              className={classes.menuOption}
+            >
+              <img src={logo} alt="Warthog logo" />
+            </NavLink>
+            <NavLink
+              tag={Link}
+              to="/home"
+              className={classes.menuOption}
+              activeClassName={classes.menuOptionActive}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              tag={Link}
+              to="/reviews"
+              className={classes.menuOption}
+              activeClassName={classes.menuOptionActive}
+            >
+              Reviews
+            </NavLink>
+            <ButtonGroup
+              size="small"
+              aria-label="small outlined button group"
+              className={classes.buttonGroup}
+            >
+              <Button>
+                <NavLink tag={Link} to="/login">
+                  Sign in
+                </NavLink>
+              </Button>
+            </ButtonGroup>
+          </Toolbar>
+        </AppBar>
         <Route exact path="/" render={() => (<Redirect to="/home" />)} />
         <Route
           path="/home"
@@ -76,4 +88,4 @@ class MainNav extends React.Component {
   }
 }
 
-export { MainNav };
+export default withStyles(styles)(MainNav);
